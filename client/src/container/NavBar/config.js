c869cad24afdd5c86ae05cd1ad4ab014cf38764b
Sayantan Pakhira <shiksayantanpakhira74@gmail.com>
2024-11-11 13:32:21 +0530

    Update project with recent changes

--- client/src/container/NavBar/config.js
+++ client/src/container/NavBar/config.js
@@ -1,8 +1,38 @@
 export const data = [{
     id: 1,
-    item: 'OTC Medicine',
+    item: 'Book Appointment',
 },
 {
     id: 2,
     item: 'Medicine Reminder'
-}]
+},
+{
+    id: 3,
+    item: 'Upload Prescription'
+},
+{
+    id: 4,
+    item: 'Med Price Comparision'
+},
+{
+    id: 5,
+    item: 'Old Prescription'
+},
+{
+    id: 6,
+    item: 'Health Record'
+},
+{
+    id: 7,
+    item: 'FAQs'
+},
+{
+    id: 8,
+    item: 'Customer Support'
+},
+{
+    id: 9,
+    item: 'Raise  Issue'
+},
+
+]
