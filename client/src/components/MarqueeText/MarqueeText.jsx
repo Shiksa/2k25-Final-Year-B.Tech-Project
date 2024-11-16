a5064e46db7f75e550f676f93db6aae3935e292f
Ashish <m.manna.bidyut@gmail.com>
2024-11-17 00:49:04 +0530

    feat: more section added

---
+++ client/src/components/MarqueeText/MarqueeText.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './MarqueeText.module.css'
+
+const MarqueeText = () => {
+  return (
+    <div className={styles.root}>Hi</div>
+  )
+}
+
+export default MarqueeText
