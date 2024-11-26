863850d754cf8ccf7085a204877ff7eac94890a1
Sayantan Pakhira <shiksayantanpakhira74@gmail.com>
2024-11-26 13:56:13 +0530

    initial routing done, feature section gave color

---
+++ client/src/container/Specialization/Specialization.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Specialization = () => {
+  return (
+    <div>
+      Doctors Specialization div box will be appear here
+    </div>
+  )
+}
+
+export default Specialization
