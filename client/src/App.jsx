f4e2b0d3ad90ba7b4638bc2eebf8b29e6db4b0b8
Diptanil03 <tatai.k.2002@gmail.com>
2024-11-29 13:30:18 +0530

    initial faqs page added by kisku

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Home from './container/Home/Home'
 import Specialization from './container/Specialization/Specialization';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import FAQs from './container/FAQ/Faqs';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/specialization' element={<Specialization />} />
+        <Route path='/faqs' element={<FAQs/>} />
       </Routes>
     </Router>
   )
