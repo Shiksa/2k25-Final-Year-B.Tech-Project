fc09671bfec2f7a81f02abb9231c6706e9478c3e
Diptanil03 <tatai.k.2002@gmail.com>
2025-06-13 20:18:39 +0530

    Some minor changes

--- client/src/container/FAQ/Faqs.jsx
+++ client/src/container/FAQ/Faqs.jsx
@@ -5,11 +5,12 @@ const FAQs = () => {
   const [openIndex, setOpenIndex] = useState(null);
 
   const faqs = [
-    { question: "What is MedBuddy?", answer: "Merclink is a powerful web application tool to help patient to find the right medication easily." },
-    { question: "How can I book appointment on MedBuddy?", answer: "You can sign up and apply through our partner program." },
-    { question: "How does MedBuddy work?", answer: "MedBuddy connects patient and doctors to get the right treatment at the right time." },
-    { question: "How do you make money?", answer: "We earn revenue by charging a small fee on successful transactions." },
+    { question: "What is MedBuddy?", answer: "MedBuddy is a powerful web application tool to help patient to find the right medication easily." },
+    { question: "How can I book appointment on MedBuddy?", answer: "You have to register on our platform and go to book appointment section to book an appointment." },
+    { question: "How can I get access to MedBuddy?", answer: "You can get access to MedBuddy by registering on our platform." },
+    { question: "How does MedBuddy work?", answer: "MedBuddy connects patient and doctors to get the right treatment and medicine at the right time." },
     { question: "How do I get access to MedBuddy's network?", answer: "Register on our platform to unlock network access and benefits." },
+    {question: "How do MedBuddy help the patients?", answer:"MedBuddy give multiple options to book appointment and lots of benefits."}
   ];
 
   const toggleFAQ = (index) => {
@@ -37,7 +38,7 @@ const FAQs = () => {
       <div className={styles.faqsFooter}>
         <h3>Still have questions?</h3>
         <p>
-          Book a call with our team to learn how we can help you better manage your e-commerce business—forever.
+          Book a call with our team to learn how we can help you to get better medication—forever.
         </p>
       </div>
     </section>
